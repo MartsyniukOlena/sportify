a442f61661dbc8dda331bcc7948e10ccf1532469
Olena Martsyniuk <martsyniuko@gmail.com>
2024-05-12 09:44:31 +0000

    Add java script for sorting and back to top button

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -66,13 +68,4 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
-def product_detail(request, product_id):
-    """ A view to show individual product details """
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'products/product_detail.html', context)
+    
