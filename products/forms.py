beb2d3f0a489185a027e19c6aaba7e149627fe58
Olena Martsyniuk <martsyniuko@gmail.com>
2024-05-19 12:33:11 +0000

    Add product form to the website for admin

--- products/forms.py
+++ products/forms.py
@@ -1,10 +1,18 @@
 from django import forms
+from .models import Product, Category
 
-class RatingForm(forms.Form):
-    rating = forms.ChoiceField(choices=(
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
-    ), widget=forms.RadioSelect())
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
