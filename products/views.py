0379f90bf57be9af13195e0115268b02be510fda
Olena Martsyniuk <martsyniuko@gmail.com>
2024-05-19 13:32:19 +0000

    Add functionality to delete products for admin

--- products/views.py
+++ products/views.py
@@ -72,11 +72,10 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    """ Add a product to the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
 
+    """ Add a product to the store """
+
+    
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -119,3 +118,10 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
