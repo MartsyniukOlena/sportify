8d97af02ae33f1bab1e99dbd3e78c26d545c9ae1
Olena Martsyniuk <martsyniuko@gmail.com>
2024-05-23 07:12:04 +0000

    Remove secret key and se debug

--- sportify/settings.py
+++ sportify/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=7cq@-fm_u9i_)vp605eqzty&q@3d6^%f!hc(_pw7y%x86bavf'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sportify-159230267d12.herokuapp.com', 'localhost']
 
