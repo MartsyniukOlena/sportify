498b869106fc9678aefa44afc6d69239e0358785
Olena Martsyniuk <martsyniuko@gmail.com>
2024-06-02 19:41:19 +0000

    Update debug

--- sportify/settings.py
+++ sportify/settings.py
@@ -27,11 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sportify-159230267d12.herokuapp.com', '.gitpod.io',
-    'localhost',
-    '127.0.0.1',]
+    'localhost', ]
 
 # Application definition
 
