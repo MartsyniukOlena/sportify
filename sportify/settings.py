06673dae8a49e88e216c31ea8014ad452939b816
Olena Martsyniuk <martsyniuko@gmail.com>
2024-06-03 13:22:12 +0000

    Add subscription form witj Mailchimp, delete not used images

--- sportify/settings.py
+++ sportify/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['sportify-159230267d12.herokuapp.com', '.gitpod.io',
     'localhost', ]
