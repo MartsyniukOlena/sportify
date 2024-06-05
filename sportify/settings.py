70f29e2a7c2e0aeef9f2af2c9e5198c9e19978b8
Olena Martsyniuk <martsyniuko@gmail.com>
2024-06-05 13:43:08 +0000

    add label to sorting, update star color for avg rating

--- sportify/settings.py
+++ sportify/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sportify-159230267d12.herokuapp.com', '.gitpod.io',
     'localhost', ]
